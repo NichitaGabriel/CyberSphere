ecb8774bab18917d63cc268694b3d29583c77173
Gabriel <gabinichita1999@gmail.com>
2024-08-01 12:33:11 +0300

    added about page

--- about.php
+++ about.php
@@ -79,27 +79,60 @@
 
   <h3 class="my-5 fw-bold g-font text-center">MANAGEMENT TEAM</h3>
 
-  <div class="swiper mySwiper">
-    <div class="swiper-wrapper">
-      <div class="swiper-slide">Slide 1</div>
-      <div class="swiper-slide">Slide 2</div>
-      <div class="swiper-slide">Slide 3</div>
-      <div class="swiper-slide">Slide 4</div>
-      <div class="swiper-slide">Slide 5</div>
-      <div class="swiper-slide">Slide 6</div>
-      <div class="swiper-slide">Slide 7</div>
-      <div class="swiper-slide">Slide 8</div>
-      <div class="swiper-slide">Slide 9</div>
+  <div class="container px-4">
+    <div class="swiper mySwiper">
+      <div class="swiper-wrapper mb-5">
+        <div class="swiper-slide bg-white text-center overflow-hidden rounded">
+          <img src="resources/images/about/IMG_17352.jpg" class="w-100">
+          <h5 class="mt-2">Random Name</h5>
+        </div>
+        <div class="swiper-slide bg-white text-center overflow-hidden rounded">
+          <img src="resources/images/about/IMG_17352.jpg" class="w-100">
+          <h5 class="mt-2">Random Name</h5>
+        </div>
+        <div class="swiper-slide bg-white text-center overflow-hidden rounded">
+          <img src="resources/images/about/IMG_17352.jpg" class="w-100">
+          <h5 class="mt-2">Random Name</h5>
+        </div>
+        <div class="swiper-slide bg-white text-center overflow-hidden rounded">
+          <img src="resources/images/about/IMG_17352.jpg" class="w-100">
+          <h5 class="mt-2">Random Name</h5>
+        </div>
+        <div class="swiper-slide bg-white text-center overflow-hidden rounded">
+          <img src="resources/images/about/IMG_17352.jpg" class="w-100">
+          <h5 class="mt-2">Random Name</h5>
+        </div>
+        <div class="swiper-slide bg-white text-center overflow-hidden rounded">
+          <img src="resources/images/about/IMG_17352.jpg" class="w-100">
+          <h5 class="mt-2">Random Name</h5>
+        </div>
+      </div>
+      <div class="swiper-pagination"></div>
     </div>
-    <div class="swiper-pagination"></div>
   </div>
 
   <?php require("inc/footer.php"); ?>
 
+  <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
   <script>
     var swiper = new Swiper(".mySwiper", {
+      spaceBetween: 40,
       pagination: {
         el: ".swiper-pagination",
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+        },
+        640: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 3,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
       },
     });
   </script>
