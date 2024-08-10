431ced09816bf0926b8658dbf4f64f3fdb2c36f6
Gabriel <gabinichita1999@gmail.com>
2024-08-10 17:00:06 +0300

    added more settings

--- admin/ajax/settings_crud.php
+++ admin/ajax/settings_crud.php
@@ -22,9 +22,26 @@ if (isset($_POST['update_general'])) {
 
 if (isset($_POST['update_shutdown'])) {
   $frm_data = ($_POST['update_shutdown'] == 0) ? 1 : 0;
- 
+
   $q = "UPDATE `settings` SET `shutdown`=? WHERE `sr_no`=?";
   $values = [$frm_data, 1];
   $res = update($q, $values, 'ii');
+  echo $res;
+}
+
+if (isset($_POST['get_contacts'])) {
+  $q = "SELECT * FROM `contact_details` WHERE `sr_no`=?";
+  $values = [1];
+  $res = select($q, $values, "i");
+  $data = mysqli_fetch_assoc($res);
+  $json_data = json_encode($data);
+  echo $json_data;
+}
+
+if (isset($_POST['update_contacts'])) {
+  $frm_data = filteration($_POST);
+  $q = "UPDATE `contact_details` SET `address`=?,`gmap`=?,`pn1`=?,`pn2`=?,`email`=?,`fb`=?,`insta`=?,`tw`=?,`iframe`=? WHERE `sr_no`=?";
+  $values = [$frm_data['address'], $frm_data['gmap'], $frm_data['pn1'], $frm_data['pn2'], $frm_data['email'], $frm_data['fb'], $frm_data['insta'], $frm_data['tw'], $frm_data['iframe'], 1];
+  $res = update($q, $values, 'sssssssssi');
   echo $res;
 }
